4c13243fe90bb60086558ed982a633de07217b46
pinkman007252 <218556900+pinkman007252@users.noreply.github.com>
2025-07-14 09:43:01 +0530

    update to git

--- src/main/java/com/java/workshop/stream/ArrayStream.java
+++ src/main/java/com/java/workshop/stream/ArrayStream.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 
 public class ArrayStream {
     public static void main(String[] args) {
-        int[] marks = { 70, 20, 90, 40, 80 };
+        int[] marks = { 70, 20, 50, 40, 80 };
 
         long numberOfSubjectsPassed = Arrays.stream(marks).filter(mark -> mark > 30).count();
         System.out.println( numberOfSubjectsPassed);
